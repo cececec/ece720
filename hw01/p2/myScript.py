e9808ec7ecf48795e431885376119a623ed8cde7
Ganqin Huang <ghuang2@grendel1.ece.ncsu.edu>
2014-01-11 17:36:41 -0500

    p1 & p2 done

---
+++ hw01/p2/myScript.py
@@ -0,0 +1,15 @@
+import re, os
+
+subdirs=os.listdir('./')
+hassubdir=0
+
+for line in subdirs:
+    m=re.search(r"^subdir$",line)
+    if m:
+        hassubdir=1
+        break
+
+if not hassubdir:
+    os.mkdir('subdir')
+
+
